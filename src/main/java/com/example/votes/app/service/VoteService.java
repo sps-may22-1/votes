a18ef1d9a93f0506ef19b2989185b161e35f7264
Maks Atygaev <atygaev.mi@gmail.com>
2022-05-14 22:23:08 +0700

    Cache vote stats

--- src/main/java/com/example/votes/app/service/VoteService.java
+++ src/main/java/com/example/votes/app/service/VoteService.java
@@ -4,13 +4,20 @@ import com.example.votes.app.domain.Vote;
 import com.example.votes.app.domain.VoteStats;
 import com.example.votes.app.repository.VoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
 public class VoteService {
 
+    private static final String VOTE_STATS_CACHE_NAME = "vote-stats";
+
     @Autowired
     private VoteRepository repository;
+
+    @Autowired
+    private CacheManager cacheManager;
 
     public boolean save(Vote vote) {
         if (repository.existsByUserId(vote.getUserId())) {
@@ -19,9 +26,12 @@ public class VoteService {
 
         repository.save(vote);
 
+        cacheManager.getCache(VOTE_STATS_CACHE_NAME).clear();
+
         return true;
     }
 
+    @Cacheable(VOTE_STATS_CACHE_NAME)
     public VoteStats getStats() {
         return repository.getStats();
     }
